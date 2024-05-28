3831956d587c85dc8975f3d05d1cc421f55b7c1b
Strxct <60505346+calvinhofman@users.noreply.github.com>
2024-05-28 11:10:05 +0200

    small push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,42 @@
 </script>
 
 <template>
-  <main>
-    <div class="bg-red-600 container">ben</div>
+  <main class="max-w-5xl mx-auto mt-4 text-white">
+    <header>
+      <nav class="flex flex-row items-center justify-between">
+        <div>
+          <h2 class="font-bold text-3xl">AnimalSelect</h2>
+        </div>
+        <ul class="flex flex-row gap-x-4 font-bold">
+          <li class="hover:text-[#bebebe]">Aanbiedingen</li>
+          <li class="hover:text-[#bebebe]">Over ons</li>
+        </ul>
+      </nav>
+    </header>
+    <section class="flex gap-x-10 flex-row items-center justify-between max-w-5xl mx-auto mt-40">
+      <div class="w-11/12 ">
+        <h2 class=" font-bold text-4xl mt-40 ">Ontdek welk dier bij jou past met de AnimalSelect tool </h2>
+        <button class="bg-[#AF62AA] p-2 rounded-lg flex justify-end ml-auto bottom-0 mt-16 text-2xl font-bold">Welk dier
+          past bij mij?</button>
+      </div>
+      <div class="w-10/12"><img src="../assets/headerImage.png" class=" rounded-lg" alt=""></div>
+    </section>
+    <section class="flex gap-x-10 flex-row-reverse items-center justify-between max-w-5xl mx-auto mt-40">
+      <div class="w-11/12 ">
+        <h2 class=" font-bold text-4xl text-center ">Waarom AnimalSelect?</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.</p>
+      </div>
+      <div class="w-10/12"><div class="w-[400px] h-[400px] rounded-lg bg-gray-400"></div></div>
+    </section>
+    <section class="flex gap-x-10 pb-32 flex-row-reverse items-center justify-center max-w-5xl mx-auto mt-40">
+      <img src="../assets/flex-row-1.png" alt="">
+      <img src="../assets/flex-row-2.png" alt="">
+      <img src="../assets/flex-row-3.png" alt="">
+      <img src="../assets/flex-row-4.png" alt="">
+    </section>
   </main>
 </template>
