111a842fafd00db229ab925fa7771ccf921f6939
Martijn-Kalkman <kalkmanmartijn@gmail.com>
2024-06-24 15:31:41 +0200

    front-end

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import ChatBox from '../views/ChatBoxView.vue';
+import ChatBox from "../views/ChatBoxView.vue";
 </script>
 
 <template>
@@ -15,29 +15,52 @@ import ChatBox from '../views/ChatBoxView.vue';
         </ul>
       </nav>
     </header>
-    <section class="flex gap-x-10 flex-row items-center justify-between max-w-5xl mx-auto mt-40">
+    <section
+      class="flex gap-x-10 flex-row items-center justify-between max-w-5xl mx-auto mt-40"
+    >
       <div class="w-11/12">
-        <h2 class="font-bold text-4xl mt-40">Ontdek welk dier bij jou past met de AnimalSelect tool</h2>
-        <button class="bg-[#AF62AA] p-2 rounded-lg flex justify-end ml-auto bottom-0 mt-16 text-2xl font-bold">Welk dier past bij mij?</button>
+        <h2 class="font-bold text-4xl mt-40">
+          Ontdek welk dier bij jou past met de AnimalSelect tool
+        </h2>
+        <button
+          class="bg-[#AF62AA] p-2 rounded-lg flex justify-end ml-auto bottom-0 mt-16 text-2xl font-bold"
+        >
+          Welk dier past bij mij?
+        </button>
       </div>
       <div class="w-10/12">
-        <img src="../assets/headerImage.png" class="rounded-lg" alt="">
+        <img src="../assets/headerImage.png" class="rounded-lg" alt="" />
       </div>
     </section>
-    <section class="flex gap-x-10 flex-row-reverse items-center justify-between max-w-5xl mx-auto mt-40">
+    <section
+      class="flex gap-x-10 flex-row-reverse items-center justify-between max-w-5xl mx-auto mt-40"
+    >
       <div class="w-11/12">
         <h2 class="font-bold text-4xl text-center">Waarom AnimalSelect?</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>
+          AnimalSelect is de perfecte app voor mensen die een huisdier willen,
+          maar weinig geld hebben. Deze slimme tool helpt je een huisdier te
+          vinden dat goed bij je past en betaalbaar is. AnimalSelect kijkt naar
+          de kosten voor eten, verzorging en doktersbezoek, en geeft advies over
+          huisdieren die niet veel geld kosten. Of je nu in een klein huis woont
+          of een druk leven hebt, AnimalSelect vindt het ideale huisdier voor
+          jou. Kies voor AnimalSelect en vind een lieve vriend zonder je zorgen
+          te maken over de kosten!
+        </p>
       </div>
       <div class="w-10/12">
-        <div class="w-[400px] h-[400px] rounded-lg bg-gray-400"></div>
+        <div class="w-[400px] h-[300px] rounded-lg">
+          <img class="rounded-xl w-full cover" src="../assets/dog-cat.jpg" />
+        </div>
       </div>
     </section>
-    <section class="flex gap-x-10 pb-32 flex-row-reverse items-center justify-center max-w-5xl mx-auto mt-40">
-      <img src="../assets/flex-row-1.png" alt="">
-      <img src="../assets/flex-row-2.png" alt="">
-      <img src="../assets/flex-row-3.png" alt="">
-      <img src="../assets/flex-row-4.png" alt="">
+    <section
+      class="flex gap-x-10 pb-32 flex-row-reverse items-center justify-center max-w-5xl mx-auto mt-40"
+    >
+      <img src="../assets/flex-row-1.png" alt="" />
+      <img src="../assets/flex-row-2.png" alt="" />
+      <img src="../assets/flex-row-3.png" alt="" />
+      <img src="../assets/flex-row-4.png" alt="" />
     </section>
   </main>
 
